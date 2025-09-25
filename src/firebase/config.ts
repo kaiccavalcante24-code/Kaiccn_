8e8bb39492ade31e3ea9110d043c3a827136047f
Kaic Cavalcante <kaiccavalcante24@gmail.com>
2025-09-25 15:31:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5062861212-d75ef",
+  "appId": "1:659452753:web:58a1d201ddf8aec03f5970",
+  "apiKey": "AIzaSyD5icMa06f63l4xWrnj1PSLVva_vxozOmo",
+  "authDomain": "studio-5062861212-d75ef.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "659452753"
+};
